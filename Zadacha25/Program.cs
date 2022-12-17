2011a6a9693df781ef39f7c9d9bcd73f5b027921
DESKTOP-KNEF2HR <stranniknochi@mail.ru>
2022-12-17 18:58:10 +0800

    first commit

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int rezult = A;
+
+for (int i = 1; i < B; i++)
+{
+    rezult *= A;
+}
+Console.WriteLine("Результат: " + rezult);
---
+++ Zadacha29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] GetArray(){
+    int [] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    Console.WriteLine(array[7]);
+    return array;
+}
+Console.WriteLine("Ведите последовательно 8 чисел: ");
+var N = string.Join(", ", GetArray());
+Console.WriteLine("[" + N + "]");
